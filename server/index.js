a48566aa02319465f1859789bb30a67a49318ccb
Alisa Lu <alisayiranlu@gmail.com>
2026-04-21 13:47:27 -0500

    fixed json error

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/receipts', receiptsRouter);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err.message || 'Internal server error' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
